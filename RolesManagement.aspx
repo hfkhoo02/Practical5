58793d975f2a82fa2557d35e86e6bd5d76e8f503
tarumt <tarumt@B32>
2023-12-12 16:14:49 +0800

    commit

---
+++ RolesManagement.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RolesManagement.aspx.cs" Inherits="Practical5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Role"></asp:Label>
+            <asp:TextBox ID="txtRole" runat="server"></asp:TextBox>
+            <asp:Button ID="btnInsertRole" runat="server" Text="Insert Role" OnClick="Button1_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Role List:"></asp:Label>
+            <br />
+            <asp:ListBox ID="lstRoles" runat="server" AutoPostBack="True"></asp:ListBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="User List:"></asp:Label>
+            <br />
+            <asp:ListBox ID="lstUsers" runat="server"></asp:ListBox>
+            
+            <br />
+            <br />
+            <asp:Button ID="Button2" runat="server" Text="Assing Role to User" OnClick="Button2_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Role"></asp:Label>
+&nbsp;<asp:DropDownList ID="ddlRole" runat="server">
+            </asp:DropDownList>
+&nbsp;<asp:Button ID="btnGetRoleUsers" runat="server" Text="Get User" OnClick="btnGetRoleUsers_Click" />
+            <br />
+            User-Role List:<br />
+            <asp:ListBox runat="server" ID="lstUsersRoles"></asp:ListBox>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ RolesManagement.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace Practical5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lstRoles.DataSource = Roles.GetAllRoles();
+                lstRoles.DataBind();
+
+                lstUsers.DataSource = Membership.GetAllUsers();
+                lstUsers.DataBind();
+
+                ddlRole.DataSource = Roles.GetAllRoles();
+                ddlRole.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string roleName = txtRole.Text.Trim();
+            try
+            {
+                if (Roles.RoleExists(roleName))
+                {
+                    
+                }
+                else
+                {
+                    Roles.CreateRole(roleName);
+                    lstRoles.DataSource = Roles.GetAllRoles();
+                    lstRoles.DataBind();
+                    ddlRole.DataSource = Roles.GetAllRoles();
+                    ddlRole.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Roles.AddUserToRole(lstUsers.SelectedItem.Text, lstRoles.SelectedItem.Text);
+
+                lstUsersRoles.DataSource =
+                    Roles.GetRolesForUser(lstUsers.SelectedItem.Text);
+                lstUsersRoles.DataBind();
+            } catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+        }
+
+        protected void btnGetRoleUsers_Click(object sender, EventArgs e)
+        {
+            lstUsersRoles.DataSource = Roles.GetUsersInRole(ddlRole.SelectedItem.Text);
+            lstUsersRoles.DataBind();   
+        }
+    }
+}
